749ca210f416d3dc5cc195b401d09bb76426fb57
alikaevahmet <alikaevahmet@gmail.com>
2025-03-26 18:14:59 +0300

    add snippetPage

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from .models import Snippet
+from django.http import HttpResponseNotFound
+from django.core.exceptions import ObjectDoesNotExist
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -19,3 +21,15 @@ def snippets_page(request):
         'pagename': 'Просмотр сниппетов',
         }
     return render(request, 'pages/view_snippets.html', context)
+
+def snippet_page(request, snippetId):
+    try:
+        snippet = Snippet.objects.get(pk=snippetId)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Snippet with id={snippetId} not found')
+    else:
+        context = {
+            'snippet': snippet,
+            'pagename': 'Просмотр сниппета',
+            }
+    return render(request, 'pages/snippet_page.html', context)
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index_page, name='index'),
     path('snippets/add', views.add_snippet_page, name='add-snippet'),
     path('snippets/list', views.snippets_page, name='view-snippet'),
+    path('snippets/<int:snippetId>', views.snippet_page, name='snippet-page')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
